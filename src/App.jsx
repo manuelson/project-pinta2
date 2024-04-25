3234a56470ac9f54a6bed51f0a83d81e5d95d6fb
Manu <manu.gonzalez@vicio.com>
2024-04-25 15:54:02 +0200

    app socket io

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import socketIO from "socket.io-client";
+
+function App() {
+
+  const socket = socketIO.connect(import.meta.env.VITE_SOCKET_IO_URL);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('messageResponse', (data) => setMessages([...messages, data]));
+  }, [socket, messages]);
+
+
+  const handleSendMessage = () => {
+    socket.emit('message', {
+      text: 'test',
+      name: 'manu',
+      id: `${socket.id}`
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleSendMessage}>HOli</button>
+      {messages.map((message, index) => (
+        <div key={index}>{message.name} - {message.text}</div>
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
