70b5987a23169e3464c799f8834bc1869a14dbb8
Manu <manu.gonzalez@vicio.com>
2024-05-02 17:14:30 +0200

    Refactoring with tailwind and context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AuthProvider from "src/context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider.Provider>
+    <App />
+  </AuthProvider.Provider>
+);
